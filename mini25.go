d95f6398ee04e65c4c19fe49d489e5765309e029
Nikitat0 <nikitatzero@yandex.ru>
2024-04-04 10:13:29 +0700

    mini25: Fix

--- mini25.go
+++ mini25.go
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/trim-a-binary-search-tree/submissions/1215393288
+// https://leetcode.com/problems/trim-a-binary-search-tree/submissions/1215941847
 
 package mini25
 
@@ -12,17 +12,13 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if low <= root.Val && root.Val <= high {
-		root.Left = trimBST(root.Left, low, high)
-		root.Right = trimBST(root.Right, low, high)
-		return root
-	}
-	for _, repl := range []*TreeNode{root.Left, root.Right} {
-		repl = trimBST(repl, low, high)
-		if repl == nil {
-			continue
-		}
-		return repl
-	}
-	return nil
+    if root.Val < low {
+        return trimBST(root.Right, low, high)
+    }
+    if high < root.Val {
+        return trimBST(root.Left, low, high)
+    }
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
 }
