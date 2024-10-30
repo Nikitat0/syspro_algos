83ae3c4bec84b1674e02db94da0632e75c71c5de
Nikitat0 <nikitatzero@yandex.ru>
2024-10-30 23:57:34 +0700

    mini24: Solve

---
+++ mini42/src/root.zig
@@ -0,0 +1,157 @@
+const std = @import("std");
+const testing = std.testing;
+const Allocator = std.mem.Allocator;
+const Random = std.Random;
+const assert = std.debug.assert;
+
+const CartesianTree = @import("./tree.zig").CartesianTree;
+
+pub fn CartesianTreeArray(comptime T: type, comptime op: fn (T, T) T) type {
+    return struct {
+        allocator: Allocator,
+        random: Random,
+        tree: Tree = Tree.empty,
+
+        const Self = @This();
+        const Tree = CartesianTree(T, op);
+
+        pub fn init(allocator: Allocator, random: Random) Self {
+            return .{ .allocator = allocator, .random = random };
+        }
+
+        pub fn len(self: Self) usize {
+            return self.tree.size();
+        }
+
+        pub fn query(self: *Self, from: usize, to: usize) T {
+            assert(from <= to and to < self.len());
+            const parts = self.tree.split(from);
+            const rightParts = parts[1].split(to - from + 1);
+            defer self.tree = parts[0].merge(rightParts[0]).merge(rightParts[1]);
+            return rightParts[0].query().?;
+        }
+
+        pub fn append(self: *Self, value: T) Allocator.Error!void {
+            try self.insert(self.len(), value);
+        }
+
+        pub fn erase(self: *Self, from: usize, count: usize) void {
+            assert(0 <= from and from + count <= self.len());
+            const parts = self.tree.split(from);
+            const rightParts = parts[1].split(count);
+            rightParts[0].deinit(self.allocator);
+            self.tree = parts[0].merge(rightParts[1]);
+        }
+
+        pub fn insert(self: *Self, pos: usize, value: T) Allocator.Error!void {
+            assert(pos <= self.len());
+            const new_node = try Tree.initSingleton(self.allocator, self.random, value);
+            const parts = self.tree.split(pos);
+            self.tree = parts[0]
+                .merge(new_node)
+                .merge(parts[1]);
+        }
+
+        pub fn remove(self: *Self, idx: usize) T {
+            assert(idx < self.len());
+            defer self.erase(idx, 1);
+            return self.query(idx, idx);
+        }
+
+        pub fn deinit(self: Self) void {
+            self.tree.deinit(self.allocator);
+        }
+    };
+}
+
+pub fn RangeSumQueryArray(comptime T: type) type {
+    return CartesianTreeArray(T, struct {
+        fn sum(a: T, b: T) T {
+            return a + b;
+        }
+    }.sum);
+}
+
+pub fn RangeMinQueryArray(comptime T: type) type {
+    return CartesianTreeArray(T, struct {
+        fn min(a: T, b: T) T {
+            return @min(a, b);
+        }
+    }.min);
+}
+
+pub fn RangeMaxQueryArray(comptime T: type) type {
+    return CartesianTreeArray(T, struct {
+        fn max(a: T, b: T) T {
+            return @max(a, b);
+        }
+    }.max);
+}
+
+const CartesianTreeArrayTest = CartesianTreeArray(usize, struct {
+    fn mul(a: usize, b: usize) usize {
+        return a * b;
+    }
+}.mul);
+
+fn mulQuery(array: []const usize, from: usize, to: usize) usize {
+    assert(from <= to and to < array.len);
+    var mul = array[from];
+    for (array[from + 1 .. to + 1]) |item|
+        mul *= item;
+    return mul;
+}
+
+fn expectArray(expected: []const usize, actual: *CartesianTreeArrayTest) !void {
+    try testing.expectEqual(expected.len, actual.len());
+    for (0..expected.len) |i| for (i..expected.len) |j|
+        try testing.expectEqual(mulQuery(expected, i, j), actual.query(i, j));
+}
+
+test "empty" {
+    var random = std.Random.DefaultPrng.init(0);
+    const empty = CartesianTreeArrayTest.init(testing.allocator, random.random());
+    defer empty.deinit();
+    try testing.expectEqual(0, empty.len());
+}
+
+test "query" {
+    var random = std.Random.DefaultPrng.init(0);
+
+    const reference: []const usize = &.{ 2, 3, 5, 7, 11 };
+    var tree = CartesianTreeArrayTest.init(testing.allocator, random.random());
+    defer tree.deinit();
+    for (reference) |item|
+        try tree.append(item);
+    try expectArray(reference, &tree);
+}
+
+test "insert & append & erase & remove" {
+    const allocator = testing.allocator;
+    var random = std.Random.DefaultPrng.init(0);
+
+    var reference = std.ArrayList(usize).init(allocator);
+    try reference.appendSlice(&.{ 2, 5, 7 });
+    defer reference.deinit();
+    var tree = CartesianTreeArrayTest.init(allocator, random.random());
+    defer tree.deinit();
+    for (reference.items) |i|
+        try tree.append(i);
+    try expectArray(reference.items, &tree);
+
+    try reference.insert(1, 3);
+    try tree.insert(1, 3);
+    try expectArray(reference.items, &tree);
+
+    try reference.append(11);
+    try tree.append(11);
+    try expectArray(reference.items, &tree);
+
+    try reference.replaceRange(1, 2, &.{});
+    tree.erase(1, 2);
+    try expectArray(reference.items, &tree);
+
+    try reference.replaceRange(1, 1, &.{});
+    try testing.expectEqual(7, tree.remove(1));
+    try expectArray(reference.items, &tree);
+}
---
+++ mini42/src/tree.zig
@@ -0,0 +1,197 @@
+const std = @import("std");
+const testing = std.testing;
+const Allocator = std.mem.Allocator;
+const Random = std.Random;
+const Tuple = std.meta.Tuple;
+const assert = std.debug.assert;
+
+pub fn CartesianTree(comptime T: type, comptime op: fn (T, T) T) type {
+    return struct {
+        root: ?*Node = null,
+
+        const Self = @This();
+        const Node = CartesianTreeNode(T, op);
+
+        pub const empty: Self = .{};
+
+        pub fn initSingleton(
+            allocator: Allocator,
+            random: Random,
+            value: T,
+        ) Allocator.Error!Self {
+            return Self.fromNode(try Node.create(allocator, value, random.int(usize)));
+        }
+
+        fn fromNode(node: ?*Node) Self {
+            return .{ .root = node };
+        }
+
+        pub fn size(self: Self) usize {
+            return (self.root orelse return 0).size;
+        }
+
+        pub fn query(self: Self) ?T {
+            return if (self.root) |root| root.query else null;
+        }
+
+        fn applyLeft(self: Self, value: T) T {
+            return if (self.root) |root| op(root.query, value) else value;
+        }
+
+        fn applyRight(self: Self, value: T) T {
+            return if (self.root) |root| op(value, root.query) else value;
+        }
+
+        pub fn merge(self: Self, other: Self) Self {
+            const left_root = self.root orelse return other;
+            const right_root = other.root orelse return self;
+            return Self.fromNode(left_root.merge(right_root));
+        }
+
+        pub fn split(self: Self, boundary: usize) Tuple(&.{ Self, Self }) {
+            const original_size = self.size();
+            assert(boundary <= original_size);
+            const root = self.root orelse return .{ empty, empty };
+            const nodes = root.split(boundary);
+            return .{ Self.fromNode(nodes[0]), Self.fromNode(nodes[1]) };
+        }
+
+        pub fn clone(self: Self, allocator: Allocator) Allocator.Error!Self {
+            return if (self.root) |root| Self.fromNode(try root.clone(allocator)) else empty;
+        }
+
+        pub fn deinit(self: Self, allocator: Allocator) void {
+            if (self.root) |root| root.destroy(allocator);
+        }
+    };
+}
+
+fn CartesianTreeNode(comptime T: type, comptime op: fn (T, T) T) type {
+    return struct {
+        value: T,
+        query: T,
+        priority: usize,
+        size: usize = 1,
+        left: Tree = Tree.empty,
+        right: Tree = Tree.empty,
+
+        const Self = @This();
+        const Tree = CartesianTree(T, op);
+
+        pub fn create(allocator: Allocator, value: T, priority: usize) Allocator.Error!*Self {
+            const self = try allocator.create(Self);
+            self.* = .{
+                .value = value,
+                .query = value,
+                .priority = priority,
+            };
+            return self;
+        }
+
+        pub fn merge(self: *Self, other: *Self) *Self {
+            if (self.priority <= other.priority) {
+                self.right = self.right.merge(Tree.fromNode(other));
+                self.update();
+                return self;
+            } else {
+                other.left = Tree.fromNode(self).merge(other.left);
+                other.update();
+                return other;
+            }
+        }
+
+        pub fn split(self: *Self, boundary: usize) Tuple(&.{ ?*Self, ?*Self }) {
+            assert(boundary <= self.size);
+            if (boundary <= self.left.size()) {
+                const parts = self.left.split(boundary);
+                self.left = parts[1];
+                self.update();
+                return .{ parts[0].root, self };
+            } else {
+                const parts = self.right.split(boundary - self.left.size() - 1);
+                self.right = parts[0];
+                self.update();
+                return .{ self, parts[1].root };
+            }
+        }
+
+        fn update(self: *Self) void {
+            self.size = 1 + self.left.size() + self.right.size();
+            self.query = self.right.applyRight(self.left.applyLeft(self.value));
+        }
+
+        pub fn clone(self: *Self, allocator: Allocator) Allocator.Error!*Self {
+            const left_cloned = try self.left.clone(allocator);
+            errdefer left_cloned.deinit(allocator);
+            const right_cloned = try self.left.clone(allocator);
+            errdefer right_cloned.deinit(allocator);
+            const cloned = try allocator.create(Self);
+            cloned.* = .{
+                .value = self.value,
+                .query = self.query,
+                .priority = self.priority,
+                .size = self.size,
+                .left = left_cloned,
+                .right = right_cloned,
+            };
+            return cloned;
+        }
+
+        pub fn destroy(self: *Self, allocator: Allocator) void {
+            self.left.deinit(allocator);
+            self.right.deinit(allocator);
+            allocator.destroy(self);
+        }
+    };
+}
+
+fn NopTree(comptime T: type) type {
+    return CartesianTree(T, struct {
+        fn nop(_: T, _: T) T {
+            return undefined;
+        }
+    }.nop);
+}
+
+test "split empty" {
+    const empty = NopTree(void).empty;
+    try testing.expectEqualDeep(.{ empty, empty }, empty.split(0));
+}
+
+test "split single" {
+    const allocator = testing.allocator;
+    var random = std.Random.DefaultPrng.init(0);
+
+    const empty = NopTree(u0).empty;
+    defer empty.deinit(allocator);
+    const single = try NopTree(u0).initSingleton(allocator, random.random(), 0);
+    defer single.deinit(allocator);
+
+    const split1 = (try single.clone(allocator)).split(0);
+    defer split1[0].deinit(allocator);
+    defer split1[1].deinit(allocator);
+    try testing.expectEqualDeep(.{ empty, single }, split1);
+
+    const split2 = (try single.clone(allocator)).split(1);
+    defer split2[0].deinit(allocator);
+    defer split2[1].deinit(allocator);
+    try testing.expectEqualDeep(.{ single, empty }, split2);
+}
+
+test "merge with empty" {
+    const allocator = testing.allocator;
+    var random = std.Random.DefaultPrng.init(0);
+
+    const empty = NopTree(u0).empty;
+    defer empty.deinit(allocator);
+    const single = try NopTree(u0).initSingleton(allocator, random.random(), 0);
+    defer single.deinit(allocator);
+
+    const mergeLeft = empty.merge(try single.clone(allocator));
+    defer mergeLeft.deinit(allocator);
+    try testing.expectEqualDeep(single, mergeLeft);
+
+    const mergeRight = (try single.clone(allocator)).merge(empty);
+    defer mergeRight.deinit(allocator);
+    try testing.expectEqualDeep(single, mergeRight);
+}
